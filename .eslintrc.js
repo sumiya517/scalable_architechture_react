64fa5484bfec17de26bfce22d42e838499dc88fb
Sumiya Alam Akhi <sumiya.alam@bjitgroup.com>
2022-08-08 08:23:56 +0600

    bbuild: implement prettier and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  globals: {
+    React: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": [1, { args: "after-used", argsIgnorePattern: "^_" }],
+  },
+};
